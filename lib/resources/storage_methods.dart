812fa3fa172cab392ce8af01d0d87da4452ff4ac
beshprakash11 <beshprakashktm@gmail.com>
2023-02-21 11:59:26 +0100

    Storage method updated

--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -1,7 +1,14 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:matcher/matcher.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 class StorageMethods{
   final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  //Adding image to firebase storage
+  Future<String> uploadImageToStorage(String childName, Uint8List file, bool isPost){
+
+  }
 }

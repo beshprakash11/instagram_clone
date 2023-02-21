a79c9de01052149410be84515c914cb86a71ff1e
beshprakash11 <beshprakashktm@gmail.com>
2023-02-21 08:41:21 +0100

    Login screen updated

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,6 +5,8 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Text('Login screen'),
+    );
   }
 }

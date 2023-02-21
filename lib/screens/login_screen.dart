1e26641cfa4eb45aad7144dd4a642306fdbde707
beshprakash11 <beshprakashktm@gmail.com>
2023-02-21 10:02:35 +0100

    Login screen updated

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:instagram_clone/utils/colors.dart';
-
-class LoginScreen extends StatelessWidget {
+import 'package:instagram_clone/widgets/text_field_input.dart';
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +29,10 @@ class LoginScreen extends StatelessWidget {
               ),
               SizedBox(height: 64,),
               //text field input for email
+              TextFieldInput(
+                hintText: 'Enter your name',
+                textInputType: TextInputType.emailAddress,
+              ),
               
 
               // text field input for password

324ace7d62b38adff4315000d7e0e1d7863013cd
beshprakash11 <beshprakashktm@gmail.com>
2023-02-21 10:45:07 +0100

    Fire base auth updated

---
+++ lib/resources/auth_methods.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthMethods{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+}

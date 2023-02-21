ea08b3dd6b77d7c45a0db64a1aea1162f04992d5
beshprakash11 <beshprakashktm@gmail.com>
2023-02-21 10:49:46 +0100

    SingUpUser function added

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,7 +1,21 @@
+import 'dart:typed_data';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthMethods{
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+
+  //signup user
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    required String bio,
+    required Uint8List file
+  }) async {
+
+  }
 }

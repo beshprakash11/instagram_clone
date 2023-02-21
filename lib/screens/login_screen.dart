510a9b6f2e928a06d068d0592458e9f01166f4d3
beshprakash11 <beshprakashktm@gmail.com>
2023-02-21 09:00:50 +0100

    Login screen updated

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -11,8 +11,13 @@ class LoginScreen extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: 32),
           width: double.infinity,
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-
+              //svg images
+              //text field input for email
+              // text field input for password
+              // button login
+              // Transitioning to sigining up
             ]
           ),
         )

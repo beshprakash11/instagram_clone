521c4190039806575038e32646bb4e2cf00e661b
beshprakash11 <beshprakashktm@gmail.com>
2023-02-21 08:34:25 +0100

    Firebase for web updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_loyout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
@@ -7,6 +8,13 @@ import 'package:instagram_clone/utils/colors.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+    await Firebase.initializeApp(
+      options: FirebaseOptions(apiKey: 'AIzaSyD7GK6Tb_H5730gRlCcArYxSYOg862GBrc', appId: appId, messagingSenderId: messagingSenderId, projectId: projectId)
+    );
+  }else{
+    await Firebase.initializeApp();
+  }
   await Firebase.initializeApp();
   runApp(const MyApp());
 }

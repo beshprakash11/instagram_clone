397eb9bac540204e66492283d0c219c5f8fb191b
beshprakash11 <beshprakashktm@gmail.com>
2023-02-21 06:51:58 +0100

    dimension added

--- lib/responsive/responsive_layout_screen.dart
+++ lib/responsive/responsive_layout_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/dimensions.dart';
 
 class ResponsvieLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-      
+      builder: (context, constraints){
+        if(constraints.maxWidth > webScreenSize){
+          //Webscreen
+
+        }
+        //Mobile screen
+      },
     );
   }
 }

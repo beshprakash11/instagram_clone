a44058455fa33f7f314599cbea9d81e04b8fe814
beshprakash11 <beshprakashktm@gmail.com>
2023-02-21 06:58:04 +0100

    Responsive layout updated

--- lib/responsive/responsive_layout_screen.dart
+++ lib/responsive/responsive_layout_screen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:instagram_clone/utils/dimensions.dart';
 
 class ResponsvieLayout extends StatelessWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+
+  const ResponsvieLayout({Key? key, required this.webScreenLayout, required this.mobileScreenLayout}): super(key:  key)
 
   @override
   Widget build(BuildContext context) {
@@ -9,9 +13,11 @@ class ResponsvieLayout extends StatelessWidget {
       builder: (context, constraints){
         if(constraints.maxWidth > webScreenSize){
           //Webscreen
+          return webScreenLayout;
 
         }
         //Mobile screen
+        return mobileScreenLayout;
       },
     );
   }

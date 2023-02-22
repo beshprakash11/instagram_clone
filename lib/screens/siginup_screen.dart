d451bc9d2eaacb56f0ef6b81bd29d948a823080a
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 07:18:22 +0100

    Signup success routing to responsive layout updated

--- lib/screens/siginup_screen.dart
+++ lib/screens/siginup_screen.dart
@@ -65,7 +65,7 @@ class _SignupScreenState extends State<SignupScreen> {
     if( res != 'success'){
       showSnackBar(res, context);
     }else{
-      Navigator.of(context).push(
+      Navigator.of(context).pushReplacement(
         MaterialPageRoute(
           builder: (context) => const ResponsvieLayout(
                 mobileScreenLayout: MobileScreenLayout(),

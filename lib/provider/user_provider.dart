e423f66d57580c7dd6c0c120035ef457a884cc49
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 08:36:48 +0100

    user provider updated

--- lib/provider/user_provider.dart
+++ lib/provider/user_provider.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/models/users.dart';
+import 'package:instagram_clone/resources/auth_methods.dart';
 
 class UserProvider with ChangeNotifier{
   User? _user;
+  final AuthMethods _authMethods = AuthMethods();
   User get getIser => _user!;
 
   Future<void> refreshUser() async{
-    
+    User user = await _authMethods.getUsersDetails();
+    _user = user;
+    notifyListeners();
   }
 }

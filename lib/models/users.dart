182a73b5b5544af4feaf8ed4e86b1b8e23a935e8
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 07:27:18 +0100

    User model updated

---
+++ lib/models/users.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class User{
+  final String username;
+  final String uid;
+  final String email;
+  final String bio;
+  final String followers;
+  final String following;
+}

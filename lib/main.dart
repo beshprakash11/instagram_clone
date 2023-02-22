29500d8d2d8b5a3eb14313068196b4bf544a867c
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 08:39:13 +0100

    main file updated for multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/provider/user_provider.dart';
 import 'package:instagram_clone/responsive/mobile_screen_loyout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
@@ -34,7 +35,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        
+        ChangeNotifierProvider(create: (_) => UserProvider())
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,

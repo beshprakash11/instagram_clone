424319036153ea1ba0fba2d769f14d40a85cf4a3
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 10:21:48 +0100

    Like animation screen updated

--- lib/widgets/like_animation.dart
+++ lib/widgets/like_animation.dart
@@ -64,6 +64,9 @@ class _LikeAnimationState extends State<LikeAnimation> with SingleTickerProvider
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ScaleTransition(
+      scale: scale,
+      child: widget.child,
+    );
   }
 }

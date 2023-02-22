8bf5b0de5fccd586d60ce80c3ab92af011b0858c
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 08:06:46 +0100

    get current user from firebase

--- lib/responsive/mobile_screen_loyout.dart
+++ lib/responsive/mobile_screen_loyout.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MobileScreenLayout extends StatefulWidget {
@@ -18,7 +19,7 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
   }
 
   void getUsername() async{
-    FirebaseFirestore.instance.collection('users');
+    FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser!.uid).get();
   }
   @override
   Widget build(BuildContext context) {

aa56b1175283e4b2ca339fc20b88268a817f5d85
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 12:15:41 +0100

    Comment card added

---
+++ lib/widgets/comment_card.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CommentCard extends StatefulWidget {
+  const CommentCard({super.key});
+
+  @override
+  State<CommentCard> createState() => _CommentCardState();
+}
+
+class _CommentCardState extends State<CommentCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 16),
+      child: Row(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}

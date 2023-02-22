0532dec222257413dbc865e34bf66888426ed1ba
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 10:02:04 +0100

    post screen updaeted

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/utils.dart';
 
 class PostCard extends StatefulWidget {
   final snap;
@@ -9,6 +11,31 @@ class PostCard extends StatefulWidget {
 }
 
 class _PostCardState extends State<PostCard> {
+  int commentLen = 0;
+  bool isLikeAnimating = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCommentLen();
+  }
+
+  fetchCommentLen() async {
+    try {
+      QuerySnapshot snap = await FirebaseFirestore.instance
+          .collection('posts')
+          .doc(widget.snap['postId'])
+          .collection('comments')
+          .get();
+      commentLen = snap.docs.length;
+    } catch (err) {
+      showSnackBar(
+        context,
+        err.toString(),
+      );
+    }
+    setState(() {});
+  }
   @override
   Widget build(BuildContext context) {
     return Container();

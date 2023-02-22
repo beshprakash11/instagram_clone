97321f48c31addd72529b1d0fa8a9857ba9b8853
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 08:18:15 +0100

    webscreen updated

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class WebScreenLayout extends StatefulWidget {
@@ -14,6 +16,17 @@ class _WebScreenLayoutState extends State<WebScreenLayout> {
   void initState() {
     super.initState();
     getUsername();
+  }
+
+  void getUsername() async{
+    DocumentSnapshot snap = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get();
+    //print(snap.data());
+    setState(() {
+      username = (snap.data() as Map<String, dynamic>)['username'];
+    });
   }
   @override
   Widget build(BuildContext context) {

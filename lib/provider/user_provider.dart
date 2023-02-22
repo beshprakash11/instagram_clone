4e3893193b3a1aebc9e96480d70a582260b2193f
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 08:26:59 +0100

    User provider updated

--- lib/provider/user_provider.dart
+++ lib/provider/user_provider.dart
@@ -4,4 +4,8 @@ import 'package:instagram_clone/models/users.dart';
 class UserProvider with ChangeNotifier{
   User? _user;
   User get getIser => _user!;
+
+  Future<void> refreshUser() async{
+    
+  }
 }

d05e898a80c6a1a9ed403518d11a91c450aa144b
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 12:18:42 +0100

    circular avatar updated

--- lib/widgets/comment_card.dart
+++ lib/widgets/comment_card.dart
@@ -16,7 +16,7 @@ class _CommentCardState extends State<CommentCard> {
         children: [
           CircleAvatar(
             backgroundImage: const NetworkImage('https://images.unsplash.com/photo-1667670650496-7d4f9e0683d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
-
+            radius: 18,
           )
 
         ],

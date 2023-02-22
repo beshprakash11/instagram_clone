3c0802377d8a00afc277408eeab523129cff3e29
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 08:02:57 +0100

    Mobile screen updated to statefull widget

--- lib/responsive/mobile_screen_loyout.dart
+++ lib/responsive/mobile_screen_loyout.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
 
   @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('This is a Mobilescreen layout.'),
-      ),
-    );
+    return Container();
   }
 }

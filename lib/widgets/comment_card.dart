6b5c7fe46315b31e56e95b4991ba8a5ad66235f6
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 12:22:11 +0100

    comment card updated

--- lib/widgets/comment_card.dart
+++ lib/widgets/comment_card.dart
@@ -14,13 +14,17 @@ class _CommentCardState extends State<CommentCard> {
       padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 16),
       child: Row(
         children: [
-          CircleAvatar(
-            backgroundImage: const NetworkImage('https://images.unsplash.com/photo-1627921227616-3e7d92a17dcb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
-            radius: 18,
-          )
-
+          _buildCardAvatar(),
         ],
       ),
+    );
+  }
+
+  Widget _buildCardAvatar() {
+    return const CircleAvatar(
+      backgroundImage: NetworkImage(
+          'https://images.unsplash.com/photo-1627921227616-3e7d92a17dcb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+      radius: 18,
     );
   }
 }

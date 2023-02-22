4f848f9f2836f69653eeadc6e5bfa6ab2cfe0431
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 08:16:15 +0100

    Webscreen updated to statefull widget

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-class WebScreenLayout extends StatelessWidget {
+class WebScreenLayout extends StatefulWidget {
   const WebScreenLayout({super.key});
 
   @override
+  State<WebScreenLayout> createState() => _WebScreenLayoutState();
+}
+
+class _WebScreenLayoutState extends State<WebScreenLayout> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("This is webscreen layout."),
-      ),
-    );
+    return Container();
   }
 }

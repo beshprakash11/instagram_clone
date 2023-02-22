705df6c69675ff6113bba2137680cd647ccd4b87
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 08:43:56 +0100

    Refresh users added in responsive layout

--- lib/responsive/responsive_layout_screen.dart
+++ lib/responsive/responsive_layout_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/provider/user_provider.dart';
 import 'package:instagram_clone/utils/dimensions.dart';
+import 'package:provider/provider.dart';
 
 class ResponsvieLayout extends StatefulWidget {
   final Widget webScreenLayout;
@@ -16,6 +18,16 @@ class ResponsvieLayout extends StatefulWidget {
 }
 
 class _ResponsvieLayoutState extends State<ResponsvieLayout> {
+  @override
+  void initState() {
+    super.initState();
+    addData();
+  }
+
+  addData() async{
+    UserProvider _userProvider = Provider.of(context, listen: false);
+    await _userProvider.refreshUser();
+  }
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(

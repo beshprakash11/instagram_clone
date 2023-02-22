5b3457d8798c32897ef5c16f9698ddbf7ec9a917
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 10:19:42 +0100

    Like animation screen updated

--- lib/widgets/like_animation.dart
+++ lib/widgets/like_animation.dart
@@ -22,6 +22,18 @@ class LikeAnimation extends StatefulWidget {
 class _LikeAnimationState extends State<LikeAnimation> with SingleTickerProviderStateMixin{
   late AnimationController controller;
   late Animation<double> scale;
+
+   @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: widget.duration.inMilliseconds ~/ 2),
+    );
+    scale = Tween<double>(begin: 1, end: 1.2).animate(controller);
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return Container();

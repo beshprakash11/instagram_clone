3b249b94db479eec9767fd2c73352894828f8e62
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 10:45:29 +0100

    Comment screen updated

--- lib/responsive/comment_screen.dart
+++ lib/responsive/comment_screen.dart
@@ -12,30 +12,38 @@ class _CommentScreenState extends State<CommentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mobileBackgroundColor,
-        title: const Text('Comments'),
-        centerTitle: false,
-      ),
-      bottomNavigationBar: SafeArea(
-        child: Container(
-          height: kToolbarHeight,
-          margin: EdgeInsets.only(
-            bottom: MediaQuery.of(context).viewInsets.bottom
-          ),
-          padding: const EdgeInsets.only(
-            left: 16,
-            right: 8
-          ),
-          child: Row(
-            children: [
-              
-            ],
-          ),
-        ),
-      ),
+      appBar: _buildAppBar(),
+      bottomNavigationBar: _buildBottomNavbar(context),
       body: Center(
         child: const Text("Commet Screen"),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: mobileBackgroundColor,
+      title: const Text('Comments'),
+      centerTitle: false,
+    );
+  }
+
+  Widget _buildBottomNavbar(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        height: kToolbarHeight,
+        margin: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom
+        ),
+        padding: const EdgeInsets.only(
+          left: 16,
+          right: 8
+        ),
+        child: Row(
+          children: [
+            
+          ],
+        ),
       ),
     );
   }

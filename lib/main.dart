dafa7dd50d66bee93e0c3c14f9ca6cb449363bff
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 08:37:48 +0100

    main file updated for multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:provider/provider.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,36 +32,41 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Instagram Clone',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: mobileBackgroundColor
-      ),
-      home:  StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot){
-          if(snapshot.connectionState == ConnectionState.active){
-            if(snapshot.hasData){
-              return const ResponsvieLayout(
-                mobileScreenLayout: MobileScreenLayout(),
-                 webScreenLayout: WebScreenLayout(),
-              );
-            }else if(snapshot.hasError){
-              return const Center(child: Text('Some error occure'),);
+    return MultiProvider(
+      providers: [
+        
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Instagram Clone',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: mobileBackgroundColor
+        ),
+        home:  StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot){
+            if(snapshot.connectionState == ConnectionState.active){
+              if(snapshot.hasData){
+                return const ResponsvieLayout(
+                  mobileScreenLayout: MobileScreenLayout(),
+                   webScreenLayout: WebScreenLayout(),
+                );
+              }else if(snapshot.hasError){
+                return const Center(child: Text('Some error occure'),);
+              }
             }
-          }
-          if(snapshot.connectionState == ConnectionState.waiting){
-            return const Center(
-              child: CircularProgressIndicator(
-                color: primaryColor,
-              ),
-            );
-          }
-
-          return const LoginScreen();
-        },
-      )//ResponsvieLayout(mobileScreenLayout: MobileScreenLayout(), webScreenLayout: WebScreenLayout(),)
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return const Center(
+                child: CircularProgressIndicator(
+                  color: primaryColor,
+                ),
+              );
+            }
+    
+            return const LoginScreen();
+          },
+        )//ResponsvieLayout(mobileScreenLayout: MobileScreenLayout(), webScreenLayout: WebScreenLayout(),)
+      ),
     );
   }
 }

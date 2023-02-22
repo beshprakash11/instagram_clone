a2fbeabde06e09a352d6b4436398812c6073253f
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 09:58:01 +0100

    Post card added

---
+++ lib/widgets/post_card.dart
@@ -0,0 +1,13 @@
+class PostCard extends StatefulWidget {
+  const PostCard({super.key});
+
+  @override
+  State<PostCard> createState() => _PostCardState();
+}
+
+class _PostCardState extends State<PostCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

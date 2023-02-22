0d4853cd13d1babbb17ef5b83c012d99aabb27c7
beshprakash11 <beshprakashktm@gmail.com>
2023-02-22 10:26:42 +0100

    Post model added

---
+++ lib/models/post.dart
@@ -0,0 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Post{
+  final String description;
+  final String uid;
+  final String username;
+  final likes;
+  final String postId;
+  final DateTime datePublished;
+  final String postUrl;
+  final String profImage;
+}

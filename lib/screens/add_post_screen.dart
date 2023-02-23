313fb0c350717406fd279d762c63b586a86a08d7
beshprakash11 <beshprakashktm@gmail.com>
2023-02-23 06:43:34 +0100

    add post updated

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -81,7 +81,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
   CircleAvatar _buildLeftCaption() {
     return const CircleAvatar(
       backgroundImage: NetworkImage(
-          'https://images.unsplash.com/photo-1676030788740-b19c2f441a7f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'),
+          'https://images.unsplash.com/photo-1518495973542-4542c06a5843?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
     );
   }
 

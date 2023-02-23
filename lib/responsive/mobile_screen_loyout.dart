8244a2503d8a6a89bd618deb9d95a17137d0a6b7
beshprakash11 <beshprakashktm@gmail.com>
2023-02-23 05:41:58 +0100

    mobile screen added

--- lib/responsive/mobile_screen_loyout.dart
+++ lib/responsive/mobile_screen_loyout.dart
@@ -97,6 +97,8 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
             backgroundColor: primaryColor,
           ),
         ],
+        onTap: navigationTapped,
+        currentIndex: _page,
       ),
     );
   }

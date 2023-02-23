1fa6fcdaa64b74876ffeb00ca10092e4bca34865
beshprakash11 <beshprakashktm@gmail.com>
2023-02-23 06:09:15 +0100

    Circular avatar added

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -21,7 +21,16 @@ class _AddPostScreenState extends State<AddPostScreen> {
       appBar: _buildAppBar(),
       body: Column(
         children: [
-          
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              CircleAvatar(
+                backgroundImage: NetworkImage('https://images.unsplash.com/photo-1676030788740-b19c2f441a7f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'),
+                
+              )
+            ],
+          )
         ],
       ),
     );

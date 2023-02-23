bb89c1a780440d733daa7269c29eb4dbbac56c9d
beshprakash11 <beshprakashktm@gmail.com>
2023-02-23 06:55:31 +0100

    add post updated

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -74,10 +74,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(user.photoUrl,),
-                    ),
-                    //_buildLeftCaption(user.photoUrl,),
+                    _buildLeftCaption(user.photoUrl,),
                     _buildInputCaption(context),
                     _buildRightCaption(),
                     const Divider(),
@@ -103,8 +100,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
         child: Container(
           decoration: const BoxDecoration(
               image: DecorationImage(
-                  image: NetworkImage(
-                      'https://images.unsplash.com/photo-1676030789552-542678bed8d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'),
+                  image:MemoryImage(_file),
                   fit: BoxFit.fill,
                   alignment: FractionalOffset.topCenter)),
         ),

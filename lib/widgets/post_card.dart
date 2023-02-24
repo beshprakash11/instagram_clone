8bae8ce68e42da5d5c0bd3d502f40ab52a3b283b
beshprakash11 <beshprakashktm@gmail.com>
2023-02-24 07:12:08 +0100

    Code refactor

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -12,12 +12,14 @@ class PostCard extends StatelessWidget {
       child: Column(
         children: [
           Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16).copyWith(right: 0),
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16)
+                .copyWith(right: 0),
             child: Row(
               children: [
                 const CircleAvatar(
                   radius: 16,
-                  backgroundImage: NetworkImage('https://images.unsplash.com/photo-1622220825868-efd9154b8e73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
+                  backgroundImage: NetworkImage(
+                      'https://images.unsplash.com/photo-1622220825868-efd9154b8e73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
                 ),
                 Expanded(
                   child: Padding(
@@ -28,39 +30,39 @@ class PostCard extends StatelessWidget {
                       children: const [
                         Text(
                           'username',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold
-                          ),
+                          style: TextStyle(fontWeight: FontWeight.bold),
                         )
                       ],
-
                     ),
-
                   ),
                 ),
                 IconButton(
-                  onPressed: (){
-                    showDialog(
-                      context: context, 
-                      builder: (context) => Dialog(
-                        child: ListView(
-                          padding: const EdgeInsets.symmetric(vertical: 16),
-                          shrinkWrap: true,
-                          children: [
-                            'Delete',
-                          ].map((e) => InkWell(
-                            onTap: (){},
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                              child: Text(e),
-                            ),
-                          )).toList(),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => Dialog(
+                          child: ListView(
+                            padding: const EdgeInsets.symmetric(vertical: 16),
+                            shrinkWrap: true,
+                            children: [
+                              'Delete',
+                            ]
+                                .map(
+                                  (e) => InkWell(
+                                    onTap: () {},
+                                    child: Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 12, horizontal: 16),
+                                      child: Text(e),
+                                    ),
+                                  ),
+                                )
+                                .toList(),
+                          ),
                         ),
-                      )
-                    );
-                  }, 
-                  icon: const Icon(Icons.more_vert)
-                )
+                      );
+                    },
+                    icon: const Icon(Icons.more_vert))
               ],
             ),
           ),

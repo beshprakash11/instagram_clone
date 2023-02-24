9ab6dfb6937dc13cc19152a13ddae7dbfaaa5eae
beshprakash11 <beshprakashktm@gmail.com>
2023-02-24 06:39:47 +0100

    Post card updated

---
+++ lib/widgets/post_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PostCard extends StatelessWidget {
+  const PostCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

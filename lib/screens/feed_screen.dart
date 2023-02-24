24c64edd5a4b1109ad53e1133d0cd6cb6e2b21a2
beshprakash11 <beshprakashktm@gmail.com>
2023-02-24 06:28:49 +0100

    Feed screen updated

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -5,6 +5,8 @@ class FeedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: Center(child: Text('Feed screen'),),
+    );
   }
 }

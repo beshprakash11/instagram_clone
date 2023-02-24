7d6bc5bdb3fad57605d23f11d04773424817ccbd
beshprakash11 <beshprakashktm@gmail.com>
2023-02-24 06:33:05 +0100

    Appbar added in feed screen

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        centerTitle: false,
+        title: Text('Feed screen'),
+      ),
       body: Center(child: Text('Feed screen'),),
     );
   }

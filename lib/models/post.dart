9ce8e5905f627701670cd2e97dd05fb914de76b4
beshprakash11 <beshprakashktm@gmail.com>
2023-02-24 05:47:19 +0100

    post model updated

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -6,7 +6,7 @@ class Post {
   final String username;
   final  likes;
   final String postId;
-  final DateTime datePublished;
+  final  datePublished;
   final String postUrl;
   final String profImage;
 

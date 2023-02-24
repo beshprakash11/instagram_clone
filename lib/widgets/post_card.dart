c91ba5b554a5241d79a9b08502eb05915a6d2e9f
beshprakash11 <beshprakashktm@gmail.com>
2023-02-24 06:56:31 +0100

    Expanded widget updated

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -15,7 +15,7 @@ class PostCard extends StatelessWidget {
             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16).copyWith(right: 0),
             child: Row(
               children: [
-                CircleAvatar(
+                const CircleAvatar(
                   radius: 16,
                   backgroundImage: NetworkImage('https://images.unsplash.com/photo-1622220825868-efd9154b8e73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
                 ),

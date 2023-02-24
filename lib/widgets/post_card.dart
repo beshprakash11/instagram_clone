de197d9c790d0f734508ad810520561eb1097db2
beshprakash11 <beshprakashktm@gmail.com>
2023-02-24 06:53:18 +0100

    Circular avatar updated

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -17,7 +17,7 @@ class PostCard extends StatelessWidget {
               children: [
                 CircleAvatar(
                   radius: 16,
-                  backgroundImage: NetworkImage('https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
+                  backgroundImage: NetworkImage('https://images.unsplash.com/photo-1622220825868-efd9154b8e73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
                 ),
               ],
             ),

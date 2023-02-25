4859aabaad800a3fc43ad91e4010f77a9500dba7
beshprakash11 <beshprakashktm@gmail.com>
2023-02-25 09:15:53 +0100

    like post updated

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -35,7 +35,19 @@ class FireStoreMethods {
 
   Future<String> likePost(String postId, String uid, List likes) async {
     try {
-      
+      String res = "Some error occurred";
+      if (likes.contains(uid)) {
+        // if the likes list contains the user uid, we need to remove it
+        _firestore.collection('posts').doc(postId).update({
+          'likes': FieldValue.arrayRemove([uid])
+        });
+      } else {
+        // else we need to add uid to the likes array
+        _firestore.collection('posts').doc(postId).update({
+          'likes': FieldValue.arrayUnion([uid])
+        });
+      }
+      res = 'success';
     } catch (e) {
       print(e.toString());     
     }

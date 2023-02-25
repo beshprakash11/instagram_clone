118e9ceb3fbe1ee8678b4175d5c351952f89f217
beshprakash11 <beshprakashktm@gmail.com>
2023-02-25 06:58:01 +0100

    Profile image updated

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -18,10 +18,9 @@ class PostCard extends StatelessWidget {
                 .copyWith(right: 0),
             child: Row(
               children: [
-                const CircleAvatar(
+                CircleAvatar(
                   radius: 16,
-                  backgroundImage: NetworkImage(
-                      'https://images.unsplash.com/photo-1622220825868-efd9154b8e73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
+                  backgroundImage: NetworkImage(snap['profImage']),
                 ),
                 Expanded(
                   child: Padding(

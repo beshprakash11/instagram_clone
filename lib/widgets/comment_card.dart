323f25689db1b3c3c12a386643f22894fe9d016e
beshprakash11 <beshprakashktm@gmail.com>
2023-02-26 09:55:42 +0100

    avatar updated from network image

--- lib/widgets/comment_card.dart
+++ lib/widgets/comment_card.dart
@@ -68,9 +68,10 @@ class _CommentCardState extends State<CommentCard> {
   }
 
   Widget _buildCardAvatar() {
-    return const CircleAvatar(
+    return CircleAvatar(
       backgroundImage: NetworkImage(
-          'https://images.unsplash.com/photo-1627921227616-3e7d92a17dcb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+          widget.snap['profilePic']
+        ),
       radius: 18,
     );
   }

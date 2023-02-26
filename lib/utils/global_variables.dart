7137e7708c9c167609e9f03197edb7acb486214c
beshprakash11 <beshprakashktm@gmail.com>
2023-02-26 10:47:50 +0100

    Global variables updated and added search screen over it

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/screens/add_post_screen.dart';
 import 'package:instagram_clone/screens/feed_screen.dart';
+import 'package:instagram_clone/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
   FeedScreen(),
-  Text('search'),
+  SearchScreen(),
   AddPostScreen(),
   Text('notif'),
   Text('profile')

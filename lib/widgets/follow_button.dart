d67ab7a8c978b2e8195c3a21c00de989f6fa1349
beshprakash11 <beshprakashktm@gmail.com>
2023-02-27 06:01:02 +0100

    follow button updated

--- lib/widgets/follow_button.dart
+++ lib/widgets/follow_button.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 class FollowButton extends StatelessWidget {
-  const FollowButton({super.key});
+  final Function()? function;
+  const FollowButton({super.key, required this.function});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.only(top: 28),
+      child: TextButton(
+        onPressed: function, 
+        child: fu
+      ),
+    );
   }
 }

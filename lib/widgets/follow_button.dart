5a912264e91cf4aff7e21d5cfce649d84407b91e
beshprakash11 <beshprakashktm@gmail.com>
2023-02-27 06:14:10 +0100

    text updated

--- lib/widgets/follow_button.dart
+++ lib/widgets/follow_button.dart
@@ -6,37 +6,34 @@ class FollowButton extends StatelessWidget {
   final Color borderColor;
   final String text;
   final Color textColor;
-  const FollowButton({
-    super.key, 
-    required this.function, 
-    required this.backgroundColor,
-    required this.borderColor,
-    required this.text,
-    required this.textColor
-  });
+  const FollowButton(
+      {super.key,
+      required this.function,
+      required this.backgroundColor,
+      required this.borderColor,
+      required this.text,
+      required this.textColor});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.only(top: 28),
       child: TextButton(
-        onPressed: function, 
+        onPressed: function,
         child: Container(
           decoration: BoxDecoration(
             color: backgroundColor,
-            border: Border.all(
-              color: borderColor
-            ),
+            border: Border.all(color: borderColor),
             borderRadius: BorderRadius.circular(5),
           ),
           alignment: Alignment.center,
           child: Text(
             text,
-            style: TextStyle(
-              color: textColor
-            ),
+            style: TextStyle(color: textColor, fontWeight: FontWeight.bold),
           ),
-        )
+          width: 250,
+          height: 27,
+        ),
       ),
     );
   }

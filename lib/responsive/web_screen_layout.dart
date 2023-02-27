e51031d4bd98c1cd15a33fcd8df129bc65dcd0fa
beshprakash11 <beshprakashktm@gmail.com>
2023-02-27 08:44:39 +0100

    webscreen appbar updated

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_clone/models/users.dart' as model;
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:instagram_clone/utils/global_variables.dart';
@@ -48,6 +49,16 @@ class _WebScreenLayoutState extends State<WebScreenLayout> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: webBackgroundColor,
+        centerTitle: false,
+        title: SvgPicture.asset(
+          'assets/ic_instagram.svg',
+          color: primaryColor,
+          height: 32,
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.messenger_outline)),
+        ],
       ),
       body: PageView(
         children: homeScreenItems,

e16c8778edfa475651e43d5ec78fc222f159ab1e
beshprakash11 <beshprakashktm@gmail.com>
2023-02-27 09:19:07 +0100

    Project completed with search screen updated

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:instagram_clone/screens/profile_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/global_variables.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -56,8 +57,12 @@ class _SearchScreenState extends State<SearchScreen> {
             (snapshot.data! as dynamic).docs[index]['postUrl'],
             fit: BoxFit.cover,
           ),
-          staggeredTileBuilder: (index) => StaggeredTile.count(
-              (index % 7 == 0) ? 2 : 1, (index % 7 == 0) ? 2 : 1),
+          staggeredTileBuilder: (index) =>
+              MediaQuery.of(context).size.width > webScreenSize
+                  ? StaggeredTile.count(
+                      (index % 7 == 0) ? 1 : 1, (index % 7 == 0) ? 1 : 1)
+                  : StaggeredTile.count(
+                      (index % 7 == 0) ? 2 : 1, (index % 7 == 0) ? 2 : 1),
           mainAxisSpacing: 8,
           crossAxisSpacing: 8,
         );

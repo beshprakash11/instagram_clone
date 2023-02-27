181125cac95167db2ae1dd1b7c471ffba6a5a1cc
beshprakash11 <beshprakashktm@gmail.com>
2023-02-27 05:11:37 +0100

    Profile screen updated

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -10,6 +11,9 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: mobileBackgroundColor,
+      body: Center(child: Text("Profile screen")),
+    );
   }
 }

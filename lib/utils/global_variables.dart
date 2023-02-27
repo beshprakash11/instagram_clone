9e2ea0bf1bf08b0776a86d477c6748b61b9ea3e0
beshprakash11 <beshprakashktm@gmail.com>
2023-02-27 05:10:55 +0100

    Profile screen added in global variable

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/screens/add_post_screen.dart';
 import 'package:instagram_clone/screens/feed_screen.dart';
+import 'package:instagram_clone/screens/profile_screen.dart';
 import 'package:instagram_clone/screens/search_screen.dart';
 
 const webScreenSize = 600;
@@ -10,5 +11,5 @@ const homeScreenItems = [
   SearchScreen(),
   AddPostScreen(),
   Text('notif'),
-  Text('profile')
+  ProfileScreen()
 ];

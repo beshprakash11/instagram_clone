a581ec0b66b6fd9a9505949492665583acbd1541
beshprakash11 <beshprakashktm@gmail.com>
2023-02-27 07:25:25 +0100

    updated search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:instagram_clone/screens/profile_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -82,15 +83,20 @@ class _SearchScreenState extends State<SearchScreen> {
         return ListView.builder(
           itemCount: (snapshot.data! as dynamic).docs.length,
           itemBuilder: (context, index){
-            return ListTile(
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage(
-                  (snapshot.data! as dynamic).docs[index]['photoUrl']
-                ),
+            return InkWell(
+              onTap: () => Navigator.of(context).push(
+                MaterialPageRoute(builder: () => ProfileScreen(uid: uid)))
               ),
-
-              title: Text((snapshot.data! as dynamic).docs[index]['username']),
-
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    (snapshot.data! as dynamic).docs[index]['photoUrl']
+                  ),
+                ),
+            
+                title: Text((snapshot.data! as dynamic).docs[index]['username']),
+            
+              ),
             );
           }
         );

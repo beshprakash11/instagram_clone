50dfe111a9b092edfc453dbf2969d70be906fba5
beshprakash11 <beshprakashktm@gmail.com>
2023-02-27 06:52:14 +0100

    ProfileScreen updated in homeScrenItems

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/screens/add_post_screen.dart';
 import 'package:instagram_clone/screens/feed_screen.dart';
@@ -6,10 +7,10 @@ import 'package:instagram_clone/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
-const homeScreenItems = [
-  FeedScreen(),
-  SearchScreen(),
-  AddPostScreen(),
-  Text('notif'),
-  ProfileScreen()
+List<Widget> homeScreenItems = [
+  const FeedScreen(),
+  const SearchScreen(),
+  const AddPostScreen(),
+  const Text('notif'),
+  ProfileScreen(uuid: FirebaseAuth.instance.currentUser!.uid,)
 ];
